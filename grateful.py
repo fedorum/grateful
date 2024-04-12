05c1890ad3475dcf1f90f8cc2f03579c0bbdd893
fedorum <isaaccheah@outlook.com>
2024-04-12 16:43:20 +1000

    made code more readable and added comments

--- grateful.py
+++ grateful.py
@@ -2,54 +2,67 @@ import os
 import datetime
 import random
 
+# initialises the current date and file to store 'grateful' entries
 today = datetime.date.today().strftime("%d %B %Y")
-gfile = 'grateful.txt'
+file = 'grateful.txt'
 
-print("Welcome to the grateful.py module!")
+# prints a welcome message and a random entry (if existing)
+print("Welcome to Grateful!")
+with open(file, 'r') as g:
+    lines = g.readlines()
+    print(lines)
+if lines:
+    print("\nHere's something you were grateful for on\n")
+    print(random.choice(lines).strip("\n"))
 
+# runs a loop until the user ends the program
 while True:
-    entry = input('\n' + "What would you like to do? (entry/ view/ delete/ exit) ")
-    if entry == "entry":
-        grateful = input("What do you feel grateful about today? ")
-        if grateful > "":
-            with open(gfile, 'a') as g:
-                g.write('\n' + str(today) + ": " + str(grateful))
-            print('\n' + "New entry created!")
-    elif entry == "view":
-        print('\n' + "Here's something you were grateful for on" + '\n')
-        with open(gfile, 'r') as g:
+    command = input("\nWhat would you like to do? (entry/ view/ delete/ exit) ").lower()
+
+    # allows user to enter an entry
+    if command == "entry":
+        entry = input("What do you feel grateful about today? ")
+        while entry <= "":
+            print("That's not a valid entry! :(")
+            entry = input("Please input something: ")
+        with open(file, 'a') as g:
+            g.write("{}: {}\n".format(str(today), str(entry)))
+        print('\n' + "New entry created!")
+
+    # displays all recorded entries
+    elif command == "view":
+        with open(file, 'r') as g:
             lines = g.readlines()
-            print(random.choice(lines))
-        viewall = input("Would you like to view everything you've been grateful for? ")
-        if viewall == "Yes":
-            print('\n' + "Here's everything you've been grateful for!")
-            with open(gfile, 'r') as g:
-                print(g.read())
-        elif viewall == "No":
-            continue
-    elif entry == "delete":
-        delete = input("Which entry would you like to delete? (DD Mmm YYYY) ")
+        if not lines:
+            print("\nOh no! You haven't added any entries yet...")
+        else:
+            print("\nHere's everything you've ever been grateful for!")
+            with open(file, 'r') as g:
+                print(g.read().strip("\n"))
+
+    # allows the user to remove a previously recorded entry
+    elif command == "delete":
+        delete = input("\nInput the index of the entry you would like to delete (i.e. 5): ")
         if delete > "          ":
-            with open(gfile, 'r') as infile, open("temp.txt", 'w') as outfile:
+            with open(file, 'r') as infile, open("temp.txt", 'w') as outfile:
                 for line in infile:
                     if delete not in line.strip('\n'):
                         outfile.write(line)
-            os.replace('temp.txt', gfile)
-            print("You've deleted a record from " + delete + "!")
+            os.replace('temp.txt', file)
+            print("\nYou've deleted a record from " + delete + "!")
         else:
-            print("No records were deleted.")
+            print("\nNo records were deleted.")
             continue
-        # with open('grateful.txt', 'r+') as g:
-        #     for line in g:
+
+    # allows the user to edit a previously recorded entry
     # elif entry == "edit":
     #     edit = input("Which entry would you like to edit? (date of entry - DD MMMM YYYY) ")
         # with open('grateful.txt', 'r+') as g:
             # for line in g:
             #     if line.contains(str(edit)):
             #         print(line)
-    elif entry == "exit":
-        print("Have a great day today! 8)")
-        exit()
 
-# notes
-# add a function to overwrite existing entries
+    # exits the program
+    elif command == "exit":
+        print("\nHave a great day today! 8)")
+        exit()
